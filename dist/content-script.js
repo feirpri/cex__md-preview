0589a0c58bb023e48f36b641c8780f45b45cb7b1
linguanghui3 <linguanghui@jd.com>
2017-03-13 18:04:13 +0800

    完善markdown预览扩展

--- dist/content-script.js
+++ dist/content-script.js
@@ -1,16 +1,34 @@
 const prefix = 'fp-md';
 let renderer = (function () {
 	let render = new marked.Renderer();
+	let headerData = [];
+
 	render.code = function (code, language) {
-		console.log(language);
-		return '<pre class="hljs" lang="' + language + '">' + code + '</pre>';
+		return [
+			`<pre class="prettyprint">`,
+			language ? `<span class="${setPrefix('lang')}">${language}</span>` : '',
+			`<code class="${language ? `hljs ${language}` : 'block'}">`,
+			`${hljs.highlightAuto(code).value}`,
+			`</code></pre>`
+		].join('');
+	}
+	render.heading = function(text, level) {
+		let id = headerData.length;
+		let head = {
+			id: id,
+			text: text,
+			level: level
+		};
+		headerData.push(head);
+		headerHook(head);
+		return `<h${level}><a class="${setPrefix('anchor')}" name="${id}" >${text}</a></h${level}>`;
 	}
 	return render;
 }());
 let headers = [];
 
 function createNode(className, tagName) {
-	className = [prefix, className].join('-') || '';
+	className = setPrefix(className);
 	tagName = tagName || 'div';
 
 	let dom = document.createElement(tagName);
@@ -23,20 +41,26 @@ function isMultiMd() {
 }
 function getMdData() {
 	let body = document.body;
-	return marked(isMultiMd() ? body.innerHTML : body.childNodes[0].innerText);
+	return marked(isMultiMd() ? body.innerHTML : body.childNodes[0].innerText, {
+		renderer: renderer,
+	});
 }
 function setPrefix(str) {
-	return [prefix, str].join('-');
+	return [prefix, str].join('--');
 }
 function addClass(dom, className) {
 	dom.setAttribute('class', [dom.getAttribute('class'), className].join(' '));
 }
-function headerHook(text, level, id) {
-	headers.push({
-		text: text,
-		level: level,
-		id: id
-	});
+function headerHook(head) {
+	headers.push(head);
+}
+function getMenuData() {
+	let str = '<ul>';
+	str += headers.map(({text, level, id}) => {
+		return text ? `<li class="${setPrefix('menu--level' + level)}"><a href="#${id}">${text}</a></li>` : '';	
+	}).join('');
+	str += '</ul>';
+	return str;
 }
 
 
@@ -44,22 +68,19 @@ let doms = {
 	menu: createNode('menu'),
 	main: createNode('main'),
 };
-marked.setOptions({
-	highlight: (code) => {
-		console.log(hljs.highlightAuto(code).value);
-		return hljs.highlightAuto(code).value;
-	},
-});
 
 (function main() {
 	let mdData = getMdData();
+	let menuData = getMenuData();
 
 	addClass(document.body, ['markdown-body', setPrefix('root')].join(' '));
 	document.body.innerHTML = '';
 	document.body.appendChild(doms.menu);
 	document.body.appendChild(doms.main);
+	document.title = (headers[0] || {}).text || 'Markdown Preview--feirpri';
 
 	doms.main.innerHTML = mdData;
+	doms.menu.innerHTML = menuData;
 	hljs.initHighlightingOnLoad();
 }());
 
